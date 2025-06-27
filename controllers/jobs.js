19a44339470677d1dd86815c3fd78427ccf59b7f
torres2235 <43655957+torres2235@users.noreply.github.com>
2025-06-26 19:27:47 -0700

    fixing deleteJob controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -66,7 +66,7 @@ const deleteJob = async (req, res) => {
     params: { id: jobId },
   } = req;
 
-  const job = await Job.findOneAndRemove({
+  const job = await Job.findOneAndDelete({
     _id: jobId,
     createdBy: userId,
   });
